98510b3e340d109a2a5517a3784f47715ccb35aa
Jonny Holupchinski <jonholup@gmail.com>
2017-03-13 14:41:28 -0500

    base mode?????

--- server/public/scripts/controllers/add-hero.js
+++ server/public/scripts/controllers/add-hero.js
@@ -1,4 +1,31 @@
-app.controller('AddHeroController', function(){
+app.controller('AddHeroController', ['$http', function($http){
     console.log('Add Hero Controller loaded');
+    var self = this;
+    self.newHero = {};
+    self.heroes = { list: []};
 
+    self.addHero = function addHero(newHero) {
+ $http({
+   method: 'POST',
+   url: '/heroes',
+   data: newHero
+ }).then(function(response){
+   console.log(response);
+   getHeros();
+   newHero = {};
+
+ });
+}; // ends addPerson function
+
+function getHeros() {
+$http({
+method: 'GET',
+url: '/heroes'
+}).then(function(response) {
+console.log('this is response.data from factory',response.data);
+console.log('this is response from factory', response);
+self.heroes.list = response.data;
 });
+}
+
+}]);
